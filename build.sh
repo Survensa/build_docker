77f465e55a220a77a4fe2f9c5f0a66ef38e6d909
Suraj Seenivasan <95559513+Survensa@users.noreply.github.com>
2023-07-26 11:28:45 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 mkdir V1_2_TE2_APPS
 docker pull connectedhomeip/chip-cert-bins:5b4f8004662d00bdb111367fec7d3ea978c23372
-sudo docker run -t -v ~/V1_2_TE2_APPS:/V1_2_TE2_APPS connectedhomeip/chip-cert-bins:b89e83be43dde7a79d90823f4bc1279eb53f76de bash -c "rm -v /V1_2_TE2_APPS/*; cp -v * /V1_2_TE2_APPS/;"
+sudo docker run -t -v ~/V1_2_TE2_APPS:/V1_2_TE2_APPS connectedhomeip/chip-cert-bins:5b4f8004662d00bdb111367fec7d3ea978c23372 bash -c "rm -v /V1_2_TE2_APPS/*; cp -v * /V1_2_TE2_APPS/;"
 sudo chown -R `whoami` ~/V1_2_TE2_APPS
