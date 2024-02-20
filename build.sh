7f2e5a3fc6de4901012fb8a8e101dfa90bc57b2c
Suraj Seenivasan <suraj@graniteriverlabs.in>
2024-02-20 09:45:26 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
-mkdir V1_2_TE2_APPS
-docker pull connectedhomeip/chip-cert-bins:5b4f8004662d00bdb111367fec7d3ea978c23372
-sudo docker run -t -v ~/V1_2_TE2_APPS:/V1_2_TE2_APPS connectedhomeip/chip-cert-bins:5b4f8004662d00bdb111367fec7d3ea978c23372 bash -c "rm -v /V1_2_TE2_APPS/*; cp -v * /V1_2_TE2_APPS/;"
-sudo chown -R `whoami` ~/V1_2_TE2_APPS
+mkdir V1_3_SVE_APPS
+docker pull connectedhomeip/chip-cert-bins:9f6d627e0262e1d023986291948bb4e845be803e
+sudo docker run -t -v ~/V1_3_SVE_APPS:/V1_3_SVE_APPS connectedhomeip/chip-cert-bins:9f6d627e0262e1d023986291948bb4e845be803e bash -c "rm -v /V1_3_SVE_APPS/*; cp -v * /V1_3_SVE_APPS/;"
+sudo chown -R `whoami` ~/V1_3_SVE_APPS
